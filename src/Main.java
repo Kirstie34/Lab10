4530295558538cf9e1fab422b7e968c43e4ede10
Kirstie Liakos <Kirstie34@gmail.com>
2017-02-10 13:38:59 -0500

    Added methods and validation

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,7 @@ public class Main {
         int count = 1; //numbers car list
         int count2 = 7; //numbers quit option
 
-
-        //list of all inventory (new and used)
-        ArrayList<Car> carList = new ArrayList<Car>();
-
-        carList.add(new Car("Nikolai", "Model S", 2017, 54999.90));
-        carList.add(new Car("Fourd", "Escapade", 2017, 31999.90));
-        carList.add(new Car("Chewie", "Vette", 2017, 44989.95));
-        carList.add(new UsedCar("Hyonda", "Prior", 2015, 14795.50, 35987.6));
-        carList.add(new UsedCar("GC", "Chirpus", 2013, 8500.00, 12345.0));
-        carList.add(new UsedCar("GC", "Witherell", 2016, 14450.00, 3500.3));
+        ArrayList<Car> carList = getCars(); //list of all inventory (new and used)
 
 
         for (Car c : carList) { //prints out complete numbered car list
@@ -32,46 +23,67 @@ public class Main {
 
         boolean carPurchase = true;
 
-        int count4 = 8; //decreases bounds for if statement with every car purchase
+        int count3 = 8; //decreases bounds for if statement with every car purchase
 
         while (carPurchase) { //repeats the following until user selects quit option
 
-            count4--;
+            count3--;
 
             System.out.println();
             System.out.print("Which car would you like? ");
-            int carChoice = scan1.nextInt();
+            int carChoice = Validate.getValidInteger(0, count3 + 1);
 
-            if (carChoice > 0 && carChoice < count4) {
-                System.out.println(carList.get(carChoice - 1));
-            } else {
-                System.out.println("Have a great day!");
-                System.exit(0);
-            }
+            selectQuitInMenu(carList, count3, carChoice);
 
             System.out.print("Would you like to buy this car? ");
             char purchaseCar = scan1.next().charAt(0);
 
-            if (purchaseCar == 'y' || purchaseCar == 'Y') {
-                System.out.println("Excellent! Our finance department will be in touch shortly.");
-                System.out.println();                                  // if user selects number of car, removes car and
-                carList.remove(carChoice - 1);                  // reprints car list
+            count2 = purchaseOrQuit(count2, carList, carChoice, purchaseCar);
+        }
+    }
 
-                int count3 = 1;
-                for (Car c : carList) {
-                    System.out.println(count3++ + ". " + c.toString());
+    private static int purchaseOrQuit(int count2, ArrayList<Car> carList, int carChoice, char purchaseCar) {
+        if (purchaseCar == 'y' || purchaseCar == 'Y') {
+            System.out.println("Excellent! Our finance department will be in touch shortly.");
+            System.out.println();                                  // if user selects number of car, removes car and
+            carList.remove(carChoice - 1);                  // reprints car list
 
-                }
-
-                System.out.println(count2-- + ". " + "Quit");
-
-            } else {
-
-                System.out.println("Have a great day!");                //if user selects number of quit, exits program
-                System.exit(0);
-
+            int count4 = 1;
+            for (Car c : carList) {
+                System.out.println(count4++ + ". " + c.toString());
 
             }
+
+            System.out.println(count2-- + ". " + "Quit");
+
+        } else {
+
+            System.out.println("Have a great day!");                //if user selects number of quit, exits program
+            System.exit(0);
+
+
         }
+        return count2;
+    }
+
+    private static void selectQuitInMenu(ArrayList<Car> carList, int count3, int carChoice) {
+        if (carChoice > 0 && carChoice < count3) {
+            System.out.println(carList.get(carChoice - 1));
+        } else {
+            System.out.println("Have a great day!");
+            System.exit(0);
+        }
+    }
+
+    private static ArrayList<Car> getCars() {
+        ArrayList<Car> carList = new ArrayList<Car>();
+
+        carList.add(new Car("Nikolai", "Model S", 2017, 54999.90));
+        carList.add(new Car("Fourd", "Escapade", 2017, 31999.90));
+        carList.add(new Car("Chewie", "Vette", 2017, 44989.95));
+        carList.add(new UsedCar("Hyonda", "Prior", 2015, 14795.50, 35987.6));
+        carList.add(new UsedCar("GC", "Chirpus", 2013, 8500.00, 12345.0));
+        carList.add(new UsedCar("GC", "Witherell", 2016, 14450.00, 3500.3));
+        return carList;
     }
 }
